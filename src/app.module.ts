a5910b1fa7bcdf0448730ad0ba8570cecc9dda7f
Felipe De Castro Curto <flpcastro@MacBook-Air-de-Felipe.local>
2022-12-16 15:59:41 -0300

    fix: kafka configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
+import { MessagingModule } from '@infra/messaging/messaging.module';
 import { Module } from '@nestjs/common';
-import { AppController } from './infra/app.controller';
-import { PrismaService } from './infra/prisma.service';
+import { SendNotification } from './application/use-cases/send-notification';
+import { DatabaseModule } from './infra/database/database.module';
+import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [PrismaService],
+  imports: [HttpModule, DatabaseModule, MessagingModule],
+  providers: [SendNotification],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { KafkaConsumerService } from '@infra/messaging/kafka/kafka-consumer.service';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +9,15 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(3000);
+  const kafkaConsumerService = app.get(KafkaConsumerService);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    strategy: kafkaConsumerService,
+  });
+
+  const PORT = Number(process.env.PORT);
+
+  await app.startAllMicroservices();
+  await app.listen(PORT);
 }
 bootstrap();
